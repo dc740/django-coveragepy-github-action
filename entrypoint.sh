15e6944f5d25a6a614b5532d66f9ee22385b1c0a
tadomikikuto-bit <lucien.programmer@gmail.com>
2020-08-12 01:06:16 +0300

    changes: customization of requirements.txt file path
    1. actions.yml
    specify the new input name and change `django-app` to `django-apps`
    to convey that it can take a list of apps
    
    2. entrypoint.sh
    change `APP` to `APPS` to convey that it can take a list of apps,
    customize the coverage command to work with pytest instead and lastly
    change the installation command to take the requirements.txt path file
    from the input
    
    3. README.md
    reflect the changes made in other files

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,20 +5,21 @@ echo "#################################################"
 echo "Starting ${GITHUB_WORKFLOW}:${GITHUB_ACTION}"
 
 
-APP=$1
+APPS=$1
 MIN_COVERAGE=$2
+REQUIREMENTS_TEXT=$3
 
 # start PostgreSQL
 service postgresql start
 
 # setup run settings
-if [ -z "${APP}" ]; then
-    # coverage on everything when app is empty
+if [ -z "${APPS}" ]; then
+    # coverage on everything when APPS is empty
     APP_LOCATION="."
     VENV_NAME="virtenv1"
 else
-    APP_LOCATION=$APP
-    VENV_NAME=virtenv_$APP
+    APP_LOCATION=$APPS
+    VENV_NAME=virtenv_$APPS
 fi
 
 # init virtual environment
@@ -28,13 +29,16 @@ fi
 
 source "${GITHUB_WORKSPACE}/${VENV_NAME}/bin/activate"
 
-pip install -r requirements.txt
+# upgrade pip to the latest version
+python -m pip install --upgrade pip
+
+pip install -r $REQUIREMENTS_TEXT
 
 echo "Base setup complete. Setting up a sample DB url and running..."
 export DATABASE_URL='postgresql://ctest:coveragetest123@127.0.0.1:5432/demo'
 
 # This will automatically fail (set -e is set by default) if the tests fail, which is OK.
-coverage run --source "${APP_LOCATION}" manage.py test "${APP}"
+coverage run --source="${APP_LOCATION}" -m pytest
 
 # Now get the coverage
 COVERAGE_RESULT=`coverage report | grep TOTAL | awk 'N=1 {print $NF}' | sed 's/%//g'`
